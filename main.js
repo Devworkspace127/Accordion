506a5623cada582db73d7f0c62818268cf3f3249
Devworkspace127 <126711838+Devworkspace127@users.noreply.github.com>
2023-12-07 22:26:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach(faq => {
-  faq.addEventListener('click', () => {
+  const question = faq.querySelector('.question');
+  const openIcon = question.querySelector('.open');
+  const closeIcon = question.querySelector('.close');
+  const answer = faq.querySelector('.answer');
+
+  question.addEventListener('click', () => {
     faq.classList.toggle('active');
-  })
-})
+    
+    if (faq.classList.contains('active')) {
+      openIcon.style.display = 'none';
+      closeIcon.style.display = 'inline';
+      answer.style.maxHeight = '300px'; // Adjust the max-height value as needed
+    } else {
+      openIcon.style.display = 'inline';
+      closeIcon.style.display = 'none';
+      answer.style.maxHeight = '0';
+    }
+  });
+});
